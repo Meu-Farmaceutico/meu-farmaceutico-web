cf80b7e531736dc9f8318d23a86e17219223b037
lucas4lves <zombnfl@gmail.com>
2022-10-08 19:31:55 -0300

    comp historico vazio mais css

--- src/components/HistoricoVazio.jsx
+++ src/components/HistoricoVazio.jsx
@@ -1,11 +1,13 @@
+import '../styles/historico-vazio.css'
+
 const HistoricoVazio = () => {
     return (
         <div className="historico-vazio">
-            <h3 className="historico-vazio__heading--bold">Ah não!</h3>
+            <h3 className="historico-vazio-heading bold">Ah não!</h3>
             <h4 className="historico-vazio__heading">Lista de Tratamentos vazia</h4>
-            <p className="historico-vazio__para">Adicione um lembrete sobre medicamentos, medições ou realiza a sua primeira compra</p>
-            <button className="historico-vazio__action lembrete" >Adicionar Medicamentos</button>
-            <button className="historico-vazio__action oferta">Ir para ofertas</button>
+            <p className="historico-vazio__para">Adicione um lembrete sobre medicamentos</p>
+            <button className="historico-vazio__cta lembrete" >Adicionar Medicamentos</button>
+            <button className="historico-vazio__cta oferta">Ir para ofertas</button>
         </div>
     )
 }
